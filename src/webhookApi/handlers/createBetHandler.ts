97a02dd25c745594744ac18942e527c09f708ff7
Angad Singh <angad.singh@imedia24.de>
2024-04-28 13:46:38 +0100

    Add log.data to BetCreated.gql

--- src/webhookApi/handlers/createBetHandler.ts
+++ src/webhookApi/handlers/createBetHandler.ts
@@ -4,6 +4,7 @@ import { CreateBetEvent } from "../types/CreateBetEvent";
 const createBetHandler = (event: APIGatewayEvent) => {
   console.log(`received create bet event: ${event.body}`);
   const createBetEvent = JSON.parse(event.body || "{}") as CreateBetEvent;
+  createBetEvent.event.data.block.logs.forEach((log) => {});
   return 200;
 };
 

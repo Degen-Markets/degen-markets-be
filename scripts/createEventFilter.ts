2f8d48a7f31697804eada745b4ba5f1e360ce55a
Angad Singh <angad.singh@imedia24.de>
2024-04-28 10:54:52 +0100

    Create event filter for Alchemy

---
+++ scripts/createEventFilter.ts
@@ -0,0 +1,26 @@
+import * as fs from "fs";
+import axios from "axios";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const queryFileName = `${process.cwd()}/src/webhookApi/graphql/${process.env.EVENT}.gql`;
+const query = fs.readFileSync(queryFileName).toString();
+
+axios.post(
+	"https://dashboard.alchemy.com/api/create-webhook", {
+		network: "BASE_MAINNET",
+		webhook_type: "GRAPHQL",
+		webhook_url: "https://webhooks.degenmarkets.com/create-bet",
+		graphql_query: {
+			skip_empty_messages: true,
+			query,
+		},
+	},
+	{
+		headers: {
+			['X-Alchemy-Token']: process.env.ALCHEMY_WEBHOOK_TOKEN,
+			accept: 'application/json',
+			['content-type']: 'application/json',
+		}
+	},
+);

946f398b4c6cd268afa20182911fa5a5b13d02db
Angad Singh <angad.singh@imedia24.de>
2024-05-01 22:22:36 +0400

    Move GraphQL files to /resources

--- scripts/createEventFilter.ts
+++ scripts/createEventFilter.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const queryFileName = `${process.cwd()}/src/webhookApi/graphql/BetSettled.gql`;
+const queryFileName = `${process.cwd()}/src/resources/graphql/BetCreated.gql`;
 const query = fs.readFileSync(queryFileName).toString();
 
 axios.post(
@@ -11,7 +11,7 @@ axios.post(
   {
     network: "BASE_MAINNET",
     webhook_type: "GRAPHQL",
-    webhook_url: "https://webhooks.degenmarkets.com/settle-bet",
+    webhook_url: "https://webhooks.degenmarkets.com/notify-create-bet",
     graphql_query: {
       skip_empty_messages: true,
       query,

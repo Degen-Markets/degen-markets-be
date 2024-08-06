75592b1908535eb0ae58f084bfdeba4139c594a2
Sayand Sathish <sayandcode@gmail.com>
2024-08-06 15:21:12 +0530

    chore: Rename `users` table to `players`

--- resources/db/migrations/V004__create_players_table.sql
+++ resources/db/migrations/V004__create_players_table.sql
@@ -1,16 +1,16 @@
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS players (
     "address"               VARCHAR(42)     NOT NULL PRIMARY KEY,
     "betsParticipatedIn"    INTEGER         NOT NULL DEFAULT 0,
     "avatarUrl"             TEXT
 )
 
-CREATE INDEX IF NOT EXISTS "idx_betsParticipatedIn" ON users("betsParticipatedIn");
+CREATE INDEX IF NOT EXISTS "idx_betsParticipatedIn" ON players("betsParticipatedIn");
 
 -- Seed with bets table data
 DO $$
     BEGIN
         -- bet creators
-        INSERT INTO users (userAddress, betsParticipatedIn)
+        INSERT INTO players (userAddress, betsParticipatedIn)
         SELECT
             LOWER(creator) AS userAddress,
             COUNT(*) AS betsParticipatedIn
@@ -18,10 +18,10 @@ DO $$
         WHERE chain = 'base'
         GROUP BY LOWER(creator)
         ON CONFLICT (userAddress) DO UPDATE
-        SET betsParticipatedIn = users.betsParticipatedIn + EXCLUDED.betsParticipatedIn;
+        SET betsParticipatedIn = players.betsParticipatedIn + EXCLUDED.betsParticipatedIn;
 
         -- bet acceptors
-        INSERT INTO users (userAddress, betsParticipatedIn)
+        INSERT INTO players (userAddress, betsParticipatedIn)
         SELECT
             LOWER(acceptor) AS userAddress,
             COUNT(*) AS betsParticipatedIn
@@ -29,6 +29,6 @@ DO $$
         WHERE acceptor IS NOT NULL AND chain = 'base'
         GROUP BY LOWER(acceptor)
         ON CONFLICT (userAddress) DO UPDATE
-        SET betsParticipatedIn = users.betsParticipatedIn + EXCLUDED.betsParticipatedIn;
+        SET betsParticipatedIn = players.betsParticipatedIn + EXCLUDED.betsParticipatedIn;
     END
 $$;

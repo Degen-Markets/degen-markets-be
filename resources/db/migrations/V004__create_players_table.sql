4909223f7c345ea9d90ff2bee2b987999e4d8d04
Angad Singh <angad17@hotmail.com>
2024-08-06 12:34:48 +0100

    Add points column & remove betsParticipatedIn

--- resources/db/migrations/V004__create_players_table.sql
+++ resources/db/migrations/V004__create_players_table.sql
@@ -1,34 +1,8 @@
 CREATE TABLE IF NOT EXISTS players (
     "address"               VARCHAR(42)     NOT NULL PRIMARY KEY,
-    "betsParticipatedIn"    INTEGER         NOT NULL DEFAULT 0,
-    "avatarUrl"             TEXT
-)
+    "avatarUrl"             TEXT,
+    chain                   VARCHAR(10)     NOT NULL DEFAULT 'base',
+    points                  INTEGER         NOT NULL DEFAULT 0
+);
 
-CREATE INDEX IF NOT EXISTS "idx_betsParticipatedIn" ON players("betsParticipatedIn");
-
--- Seed with bets table data
-DO $$
-    BEGIN
-        -- bet creators
-        INSERT INTO players (userAddress, betsParticipatedIn)
-        SELECT
-            LOWER(creator) AS userAddress,
-            COUNT(*) AS betsParticipatedIn
-        FROM bets
-        WHERE chain = 'base'
-        GROUP BY LOWER(creator)
-        ON CONFLICT (userAddress) DO UPDATE
-        SET betsParticipatedIn = players.betsParticipatedIn + EXCLUDED.betsParticipatedIn;
-
-        -- bet acceptors
-        INSERT INTO players (userAddress, betsParticipatedIn)
-        SELECT
-            LOWER(acceptor) AS userAddress,
-            COUNT(*) AS betsParticipatedIn
-        FROM bets
-        WHERE acceptor IS NOT NULL AND chain = 'base'
-        GROUP BY LOWER(acceptor)
-        ON CONFLICT (userAddress) DO UPDATE
-        SET betsParticipatedIn = players.betsParticipatedIn + EXCLUDED.betsParticipatedIn;
-    END
-$$;
+CREATE INDEX IF NOT EXISTS "idx_points" ON players("points");

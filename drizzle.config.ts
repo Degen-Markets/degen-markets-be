cad8c27ab1eee08fadf8fc7d50fd9f00f39ef39d
Sayand Sathish <sayandcode@gmail.com>
2024-08-07 14:42:54 +0530

    chore: Swap existing migration files with drizzle generated ones
    The drizzle-config path was also modified to match our existing
    migrations path

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   schema: "./resources/db/schema.ts",
-  out: "./resources/db/migrations/temp",
+  out: "./resources/db/migrations",
   dialect: "postgresql",
 });
--- resources/db/migrations/V003__add_tx_columns.sql
+++
@@ -1,7 +0,0 @@
-DO $$
-    BEGIN
-        ALTER TABLE bets ADD COLUMN "paidTx" VARCHAR(66);
-    EXCEPTION
-        WHEN duplicate_column THEN RAISE NOTICE 'Column "paidTx" already exists in bets.';
-    END
-$$;

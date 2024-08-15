2e21775b9bf1ef0e6126de99b8a5538cf0703ef9
Angad Singh <angad17@hotmail.com>
2024-08-15 17:03:30 +0100

    Handle old pools

--- src/solanaActions/getPool.ts
+++ src/solanaActions/getPool.ts
@@ -5,10 +5,10 @@ import { program } from "./constants";
 import { buildBadRequestError } from "../utils/errors";
 import pools from "./pools.json";
 import { LinkedAction } from "@solana/actions-spec";
-import { deriveEntryAccountKey } from "./enterPoolTx";
-import { PublicKey } from "@solana/web3.js";
+import { PublicKey, SystemProgram } from "@solana/web3.js";
+import BN from "bn.js";
 
-const logger: Logger = new Logger({ serviceName: "getPoolService" });
+const logger: Logger = new Logger({ serviceName: "GetPoolService" });
 
 interface PoolResponse extends ActionGetResponse {
   links: {
@@ -28,13 +28,29 @@ export const getPool = async (event: APIGatewayEvent) => {
   const pool = pools[id];
 
   logger.info(`loading pool with id: ${id}`);
-  const poolAccount = await program.account.pool.fetch(id);
-  logger.info(JSON.stringify(poolAccount, null, 3));
+  let poolAccount: {
+    title: string;
+    hasConcluded: boolean;
+    value: BN;
+    winningOption: PublicKey;
+  } = {
+    title: pool.title,
+    hasConcluded: true,
+    value: new BN(0),
+    winningOption: SystemProgram.programId,
+  };
+  let description: string = "";
+  try {
+    poolAccount = await program.account.pool.fetch(id);
+    logger.info(`Pool Found: ${JSON.stringify(poolAccount, null, 3)}`);
+  } catch (e) {
+    description = "Pool ended!";
+  }
   const metadata: PoolResponse = {
     icon: pool.image,
-    label: poolAccount.title,
-    title: poolAccount.title,
-    description: "",
+    label: pool.title,
+    title: pool.title,
+    description,
     links: {
       actions: [],
     },

3f7f29dc534232b617f7331f320d5febdb1a9c8b
sayandcode <sayandcode@gmail.com>
2024-09-09 11:05:22 +0530

    refactor: Split `smartContractEventProcessor` into smaller functions
    This aids in readability and testing

--- src/smartContractEventProcessor/smartContractEventProcessor.ts
+++ src/smartContractEventProcessor/smartContractEventProcessor.ts
@@ -10,35 +10,44 @@ import { SmartContractEvent } from "./types";
 const logger = new Logger({ serviceName: "smartContractEventProcessor" });
 
 const handleSqsEvent = async (sqsEvent: SQSEvent) => {
-  for (const record of sqsEvent.Records) {
-    const eventParseTrial = tryIt(
-      () => JSON.parse(record.body) as SmartContractEvent,
-    );
-    if (!eventParseTrial.success) {
-      logger.error("Failed to parse smart contract event from record body", {
-        error: eventParseTrial.err,
-        recordBody: record.body,
-      });
-      return;
-    }
-    const smartContractEvent = eventParseTrial.data;
-
-    switch (smartContractEvent.eventName) {
-      case "poolEntered":
-        logger.info("Processing `poolEntered` event", {
-          event: smartContractEvent,
-        });
-        await poolEnteredEventHandler(smartContractEvent.data);
-        break;
-
-      default:
-        logger.warn(`${smartContractEvent.eventName} events are not handled`, {
-          event: smartContractEvent,
-        });
-    }
-  }
-
+  await Promise.all(
+    sqsEvent.Records.map(async (record) => {
+      await handleRecord(record);
+    }),
+  );
   return buildOkResponse("SQS event processor ran successfully");
+};
+
+const handleRecord = async (record: SQSEvent["Records"][0]) => {
+  const eventParseTrial = tryIt(
+    () => JSON.parse(record.body) as SmartContractEvent,
+  );
+  if (!eventParseTrial.success) {
+    logger.error("Failed to parse smart contract event from record body", {
+      error: eventParseTrial.err,
+      recordBody: record.body,
+    });
+    return;
+  }
+  await forwardToEventHandler(eventParseTrial.data);
+};
+
+const forwardToEventHandler = async (
+  smartContractEvent: SmartContractEvent,
+) => {
+  switch (smartContractEvent.eventName) {
+    case "poolEntered":
+      logger.info("Processing `poolEntered` event", {
+        event: smartContractEvent,
+      });
+      await poolEnteredEventHandler(smartContractEvent.data);
+      break;
+
+    default:
+      logger.warn(`${smartContractEvent.eventName} events are not handled`, {
+        event: smartContractEvent,
+      });
+  }
 };
 
 export const handler = middy(handleSqsEvent).use(

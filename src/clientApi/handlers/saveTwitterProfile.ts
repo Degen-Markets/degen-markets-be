bcd3e30733828d3b655b57164b130b099d3a65e1
sayandcode <sayandcode@gmail.com>
2024-09-16 14:03:31 +0530

    feat: Add points for linking twitter
    - When the user hits '/save-twitter-profile' clientApi endpoint, we check
      if they are in db. If they aren't we add them (with twitter profile data)
      and award them 10 points. If they are in the db, they still get 10 points
      if there's no twitter profile yet. But if they've already linked the
      twitter profile we just update the profile.
    
    - We moved the `findHighResImageUrl` to a shared util. This will aid in
      testing later.
    
    - Minor control flow and logging improvements were also made.

--- src/clientApi/handlers/saveTwitterProfile.ts
+++ src/clientApi/handlers/saveTwitterProfile.ts
@@ -3,42 +3,76 @@ import { buildBadRequestError } from "../../utils/errors";
 import { verifySignature } from "../../utils/cryptography";
 import { DrizzleClient } from "../../clients/DrizzleClient";
 import PlayersService from "../../players/service";
-import { buildOkResponse } from "../../utils/httpResponses";
+import { buildErrorResponse, buildOkResponse } from "../../utils/httpResponses";
 import { findConnectedUser, requestAccessToken } from "../../utils/twitter";
 import { Logger } from "@aws-lambda-powertools/logger";
+import { findHighResImageUrl } from "./utils";
 
 const logger = new Logger({ serviceName: "saveTwitterProfile" });
 
-const lowResImageSuffix = "_normal";
+const POINTS_AWARDED_FOR_LINKING_TWITTER = 10;
 
-const findHighResImageUrl = (twitterImageUrl: string | undefined) => {
-  if (!twitterImageUrl) return undefined;
-  return twitterImageUrl.split(lowResImageSuffix).join("");
+type SuccessPayload = {
+  twitterUsername?: string;
+  twitterPfpUrl?: string;
 };
 
+/**
+ * This method should return the {@linkcode SuccessPayload} in the HTTP response for happy path
+ */
 const saveTwitterProfile = async (
   event: APIGatewayProxyEventV2,
 ): Promise<APIGatewayProxyResultV2> => {
+  logger.info("Running `saveTwitterProfile` handler", { event });
   const body = JSON.parse(event.body || "{}");
   const { twitterCode, signature, address } = body;
   if (!twitterCode || !signature || !address) {
     return buildBadRequestError("Missing properties in request body");
   }
+
   const isCorrectAddress = verifySignature(signature, address);
   if (!isCorrectAddress) {
     return buildBadRequestError("This is not your address!");
   }
   logger.info(`Signature verification passed`);
+
   await requestAccessToken(twitterCode);
   const { data: twitterUser } = await findConnectedUser();
+  if (!twitterUser) {
+    logger.error("Couldn't find twitter user from code", { twitterCode });
+    return buildErrorResponse("Invalid twitter user");
+  }
   logger.info("Found user's twitter profile", { twitterUser });
+
+  // add to db
   const db = await DrizzleClient.makeDb();
-  const user = await PlayersService.insertNewOrSaveTwitterProfile(db, {
-    address,
-    twitterUsername: twitterUser?.username,
-    twitterPfpUrl: findHighResImageUrl(twitterUser?.profile_image_url),
-  });
-  return buildOkResponse(user);
+  const twitterProfile = {
+    twitterUsername: twitterUser.username,
+    twitterPfpUrl: twitterUser.profile_image_url
+      ? findHighResImageUrl(twitterUser.profile_image_url)
+      : undefined,
+  };
+  const playerInDb = await PlayersService.getPlayerById(db, address);
+  if (!playerInDb) {
+    logger.info("Player doesn't exist, creating new player");
+    await PlayersService.insertNew(db, {
+      address,
+      points: POINTS_AWARDED_FOR_LINKING_TWITTER,
+      ...twitterProfile,
+    });
+  } else {
+    const isTwitterAlreadyAdded = !!playerInDb.twitterUsername;
+    if (!isTwitterAlreadyAdded) {
+      await PlayersService.changePoints(
+        db,
+        address,
+        POINTS_AWARDED_FOR_LINKING_TWITTER,
+      );
+    }
+    await PlayersService.updateTwitterProfile(db, address, twitterProfile);
+  }
+
+  return buildOkResponse(twitterProfile satisfies SuccessPayload);
 };
 
 export default saveTwitterProfile;

4980afa1d85ebfeee4b3fb12a6fc6a6865cfdb79
Angad Singh <30783941+AngSin@users.noreply.github.com>
2024-09-16 14:05:26 +0100

    Use Drizzle for running db migrations (#27)
    Co-authored-by: Angad Singh <angadsingh@Angads-Air.Home>

--- src/dbMigration/dbMigration.ts
+++ src/dbMigration/dbMigration.ts
@@ -1,31 +1,19 @@
-import * as fs from "fs";
 import { Logger } from "@aws-lambda-powertools/logger";
 import { injectLambdaContext } from "@aws-lambda-powertools/logger/middleware";
-import { DatabaseClient } from "../clients/DatabaseClient";
 import middy from "@middy/core";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
+import { DrizzleClient } from "../clients/DrizzleClient";
 
 const logger = new Logger({ serviceName: "dbMigration" });
-const databaseClient = new DatabaseClient();
-const path = "resources/db/migrations";
 
-export const executeMigration = async () => {
-  const migrationFiles = fs.readdirSync(path);
-  logger.info("migrating files", { migrationFiles });
-  for (const f of migrationFiles) {
-    try {
-      await migrate(f);
-    } catch (e) {
-      logger.error(`failed to migrate file ${f}`, { error: e });
-    }
-  }
+export const executeMigrations = async () => {
+  logger.info("Making DB Connection");
+  const db = await DrizzleClient.makeDb();
+  logger.info("Running migrations");
+  await migrate(db, { migrationsFolder: "resources/db/migrations" }); // defined in DatabaseStack (afterBundling)
+  logger.info("Migrations completed");
 };
 
-const migrate = async (file: string) => {
-  const fileContent = fs.readFileSync(`${path}/${file}`, "utf8");
-  await databaseClient.executeStatement(fileContent);
-  console.info(`Successfully migrated ${file}`);
-};
-
-export const handler = middy(executeMigration).use(
+export const handler = middy(executeMigrations).use(
   injectLambdaContext(logger, { logEvent: true }),
 );

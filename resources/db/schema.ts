11628897d55203634215e727d319ba20e01e0014
qalbehabib <rana.habib313@gmail.com>
2024-09-20 19:50:02 +0500

    Feat(Pool & PoolOption): schema & migration added.

--- resources/db/schema.ts
+++ resources/db/schema.ts
@@ -1,5 +1,9 @@
-import { poolEntriesTable } from "../../src/poolEntries/schema";
+import {
+  poolEntriesTable,
+  poolOptionsTable,
+  poolsTable,
+} from "../../src/poolEntries/schema";
 import { playersTable } from "../../src/players/schema";
 
 // export all tables in db from this file
-export { poolEntriesTable, playersTable };
+export { poolEntriesTable, playersTable, poolOptionsTable, poolsTable };
--- src/poolEntries/schema.ts
+++ src/poolEntries/schema.ts
@@ -1,5 +1,41 @@
-import { index, numeric, pgTable, varchar } from "drizzle-orm/pg-core";
+import { index, numeric, pgTable, varchar, boolean } from "drizzle-orm/pg-core";
 import { playersTable } from "../players/schema";
+
+// Define Pools Table
+export const poolsTable = pgTable(
+  "pools",
+  {
+    id: varchar("id", { length: 44 }).primaryKey(),
+    title: varchar("title", { length: 255 }).notNull(),
+    description: varchar("description", { length: 5000 }),
+    image: varchar("image", { length: 1000 }),
+    isPaused: boolean("is_paused"), // Field can be true, false, or null (we can add .notNull() for true, false only)
+    winningOption: varchar("winning_option", { length: 44 }), // Foreign key to winning option
+    value: numeric("value", { precision: 50, scale: 0 }).notNull(), // Total value in the pool
+  },
+  (table) => {
+    return {
+      idxWinningOption: index("idx_winning_option").on(table.winningOption),
+    };
+  },
+);
+
+export const poolOptionsTable = pgTable(
+  "pool_options",
+  {
+    id: varchar("id", { length: 44 }).primaryKey(), // Option ID
+    poolId: varchar("pool_id", { length: 44 })
+      .notNull()
+      .references(() => poolsTable.id), // Foreign key to poolsTable (each option belongs to a pool)
+    title: varchar("title", { length: 255 }).notNull(), // Option title
+    value: numeric("value", { precision: 50, scale: 0 }).notNull(), // Total value in the option
+  },
+  (table) => {
+    return {
+      idxPoolId: index("idx_pool_id").on(table.poolId),
+    };
+  },
+);
 
 export const poolEntriesTable = pgTable(
   "pool_entries",
@@ -10,19 +46,20 @@ export const poolEntriesTable = pgTable(
     /** The solana address of the user who entered the bet */
     entrant: varchar("entrant", { length: 44 })
       .notNull()
-      .references(() => playersTable.address),
+      .references(() => playersTable.address), // Foreign key to playersTable
 
-    /** The solana address of the option account (PDA associated to the pools program)*/
-    option: varchar("option", { length: 44 }).notNull(),
+    /** The solana address of the option account */
+    option: varchar("option", { length: 44 })
+      .notNull()
+      .references(() => poolOptionsTable.id), // Foreign key to poolOptionsTable
 
-    /** The solana address of the pool account (PDA associated to the pools program)*/
-    pool: varchar("pool", { length: 44 }).notNull(),
+    /** The solana address of the pool account */
+    pool: varchar("pool", { length: 44 })
+      .notNull()
+      .references(() => poolsTable.id), // Foreign key to poolsTable
 
     /** The total value (money) in this specific entry */
-    value: numeric("value", {
-      precision: 50, // biggest number in rust is u128, so 50 precision gives us plenty of space
-      scale: 0, // 0 scale because we do not want any decimals
-    }).notNull(),
+    value: numeric("value", { precision: 50, scale: 0 }).notNull(), // Total value in the entry
   },
   (table) => {
     return {

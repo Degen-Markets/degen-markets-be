6fabb2323abbc93a061cbfd09a45c54f86715dd2
sayandcode <sayandcode@gmail.com>
2024-10-24 13:41:55 +0530

    feat: Support development db in `make tunnel`
    We now show the user a prompt which will help them connect to
    the database of choice

--- scripts/connect_database.sh
+++ scripts/connect_database.sh
@@ -2,12 +2,36 @@
 
 source .env
 
-export basti_id=`aws ec2 describe-instances --profile ${AWS_PROFILE} | jq -r '.Reservations[] | .Instances[] | select(.State.Name == "running") | select(.Tags[] | .Key == "Name" and .Value == "BastionHost") | .InstanceId'`
+# Prompt user to select the database
+options=("production" "development")
+echo "Select the database you want to connect to:"
+select opt in "${options[@]}"
+do
+  case $opt in
+    "production")
+      export AWS_REGION="eu-west-1";
+      break
+      ;;
+    "development")
+      export AWS_REGION="us-east-1";
+      break
+      ;;
+    *) 
+      echo "Invalid option. Please try again."
+      ;;
+  esac 
+done
+
+echo "Connecting to $opt database..."
+
+bastion_host_ec2_name=$( [ $opt == 'production' ] && echo 'BastionHost' || echo 'DevBastionHost' );
+
+export basti_id=`aws ec2 describe-instances --profile ${AWS_PROFILE} | jq -r '.Reservations[] | .Instances[] | select(.State.Name == "running") | select(.Tags[] | .Key == "Name" and .Value == "'${bastion_host_ec2_name}'" ) | .InstanceId'`
 aws ec2-instance-connect send-ssh-public-key \
        --profile ${AWS_PROFILE} \
        --instance-id ${basti_id} \
        --instance-os-user ec2-user \
-       --availability-zone `aws ec2 describe-instances --profile ${AWS_PROFILE} | jq -r '.Reservations[] | .Instances[] | select(.State.Name == "running") | select(.Tags[] | .Key == "Name" and .Value == "BastionHost") | .Placement.AvailabilityZone'` \
+       --availability-zone `aws ec2 describe-instances --profile ${AWS_PROFILE} | jq -r '.Reservations[] | .Instances[] | select(.State.Name == "running") | select(.Tags[] | .Key == "Name" and .Value == "'${bastion_host_ec2_name}'") | .Placement.AvailabilityZone'` \
        --ssh-public-key file://`echo ~`/.ssh/id_rsa.pub > /dev/null
 
 database_host=$(aws rds describe-db-instances --profile ${AWS_PROFILE} | jq -r '.DBInstances[] | select(.DBName == "degenmarkets") | .Endpoint.Address')

ecd46477c427a95ff7c0401cfffe3f6421b2ae27
Angad Singh <30783941+AngSin@users.noreply.github.com>
2024-11-06 19:05:24 +0000

    Update Solana Resources (#71)
    * Update Solana Resources
    
    * Update pool derivation
    
    * make derivePoolAccountKey non sync
    
    * Fix derivePoolAccountKey
    
    * Update idl address to DCSwWMUAWXVAz9MrezGQBVX6PpKtZBDD2UAq6Jn2KkH9

--- src/pools/utils.ts
+++ src/pools/utils.ts
@@ -1,12 +1,19 @@
 import * as anchor from "@coral-xyz/anchor";
 import { getBytesFromHashedStr } from "../utils/cryptography";
 import { programId } from "../utils/constants";
+import { Logger } from "@aws-lambda-powertools/logger";
 
-export const derivePoolAccountKey = (title: string) => {
+const logger = new Logger({ serviceName: "PoolsUtils" });
+
+export const derivePoolAccountKey = (
+  title: string,
+  creator: anchor.web3.PublicKey,
+) => {
+  logger.info(`Deriving pool account from ${title} & ${creator.toString()}`);
   const [pda] = anchor.web3.PublicKey.findProgramAddressSync(
-    [getBytesFromHashedStr(title)],
+    [getBytesFromHashedStr(title), creator.toBytes()],
     programId,
   );
-  console.log(`Derived pool account is ${pda}`);
+  logger.info(`Derived pool account key: ${pda.toString()}`);
   return pda;
 };

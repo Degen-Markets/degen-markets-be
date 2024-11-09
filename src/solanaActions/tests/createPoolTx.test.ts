fbcf2db5144f88e6c42df6c4edec3398b4007dfd
sayandcode <sayandcode@gmail.com>
2024-11-09 16:16:40 +0530

    test: Test `_Utils.getPayload`

--- src/solanaActions/tests/createPoolTx.test.ts
+++ src/solanaActions/tests/createPoolTx.test.ts
@@ -1,4 +1,5 @@
-import { ACTIONS_CORS_HEADERS, createPostResponse } from "@solana/actions";
+import { ACTIONS_CORS_HEADERS } from "@solana/actions";
+import { ActionPostResponse } from "@solana/actions-spec";
 import {
   buildBadRequestError,
   buildInternalServerError,
@@ -7,28 +8,36 @@ import {
 import createPoolTx, { _Utils } from "../createPoolTx";
 import { APIGatewayProxyEventV2 } from "aws-lambda";
 import { PublicKey } from "@solana/web3.js";
-
-const mockEvent = {
-  queryStringParameters: {
-    image: "imageUrl",
-    title: "mockTitle",
-    description: "mockDescription",
-  },
-  body: JSON.stringify({
-    account: "GzA4Tu7SpEBKHJBxww6DSqEUm3e77yzNX251c3vK8i5b",
-  }),
-} as unknown as APIGatewayProxyEventV2;
-
-const spiedProcessAndUploadImage = jest
-  .spyOn(_Utils, "processAndUploadImage")
-  .mockResolvedValue("mock-image-url");
-
-const mockPayload = "mock-payload";
-const spiedGetPayload = jest
-  .spyOn(_Utils, "getPayload")
-  .mockResolvedValue(mockPayload as any);
+import { connection } from "../../clients/SolanaProgramClient";
 
 describe("createPoolTx", () => {
+  let mockEvent: APIGatewayProxyEventV2;
+  let spiedProcessAndUploadImage: jest.SpyInstance;
+  let spiedGetPayload: jest.SpyInstance;
+  let mockPayload: string;
+
+  beforeAll(() => {
+    mockEvent = {
+      queryStringParameters: {
+        image: "imageUrl",
+        title: "mockTitle",
+        description: "mockDescription",
+      },
+      body: JSON.stringify({
+        account: "GzA4Tu7SpEBKHJBxww6DSqEUm3e77yzNX251c3vK8i5b",
+      }),
+    } as unknown as APIGatewayProxyEventV2;
+
+    spiedProcessAndUploadImage = jest
+      .spyOn(_Utils, "processAndUploadImage")
+      .mockResolvedValue("mock-image-url");
+
+    mockPayload = "mock-payload";
+    spiedGetPayload = jest
+      .spyOn(_Utils, "getPayload")
+      .mockResolvedValue(mockPayload as any);
+  });
+
   it("should return a 400 error if the title is not found", async () => {
     const badEvent = {} as APIGatewayProxyEventV2;
     const response = await createPoolTx(badEvent);
@@ -93,5 +102,34 @@ describe("createPoolTx", () => {
     expect(response).toEqual(
       buildOkResponse(mockPayload, ACTIONS_CORS_HEADERS),
     );
+  });
+});
+
+describe("getPayload", () => {
+  beforeAll(() => {
+    jest.restoreAllMocks();
+    jest.spyOn(connection, "getLatestBlockhash").mockResolvedValue({
+      blockhash: "GzA4Tu7SpEBKHJBxww6DSqEUm3e77yzNX251c3vK8i5b",
+      lastValidBlockHeight: 1,
+    });
+  });
+
+  it("returns the payload if successful", async () => {
+    const payload = await _Utils.getPayload({
+      poolTitle: "mockTitle",
+      imgUrl: "mockImageUrl",
+      description: "mockDescription",
+      creator: new PublicKey("GzA4Tu7SpEBKHJBxww6DSqEUm3e77yzNX251c3vK8i5b"),
+    });
+
+    const expectedPayload: ActionPostResponse = {
+      type: "transaction",
+      transaction: expect.any(String),
+      links: {
+        next: expect.any(Object),
+      },
+    };
+
+    expect(payload).toEqual(expectedPayload);
   });
 });

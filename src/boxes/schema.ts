c23d4ba255bcb5cef8aaf0117970c5f6159f0567
Abdelhak Ahzab <abdou.ahzab@gmail.com>
2024-11-19 16:24:57 +0100

    Change winning_amount from decimal to integer

--- src/boxes/schema.ts
+++ src/boxes/schema.ts
@@ -4,23 +4,18 @@ import {
   pgTable,
   timestamp,
   varchar,
-  decimal,
+  integer,
 } from "drizzle-orm/pg-core";
 
 export const boxesTable = pgTable(
   "boxes",
   {
     isOpened: boolean("is_opened").notNull().default(false),
-
     player: varchar("player", { length: 44 }).notNull(),
-
     createdAt: timestamp("created_at").notNull().defaultNow(),
-
     openedAt: timestamp("opened_at"),
-
     winningToken: varchar("winning_token", { length: 44 }),
-
-    winningAmount: decimal("winning_amount", { precision: 18, scale: 9 }),
+    winningAmount: integer("winning_amount"),
   },
   (table) => ({
     idxPlayer: index("idx_player").on(table.player),

fad0e8b035786566f9bd7b79468a8f72af9d401d
angadsingh <angad17@hotmail.com>
2024-11-20 18:20:16 +0000

    use OPENAI_API_KEY

--- src/aiTweeter/aiTweeter.ts
+++ src/aiTweeter/aiTweeter.ts
@@ -2,7 +2,11 @@ import { ScheduledEvent } from "aws-lambda";
 import { Logger } from "@aws-lambda-powertools/logger";
 import OpenAI from "openai";
 import { sendBotTweet } from "../utils/twitterBot";
-const openai = new OpenAI();
+import { getMandatoryEnvVariable } from "../utils/getMandatoryEnvValue";
+
+const openai = new OpenAI({
+  apiKey: getMandatoryEnvVariable("OPENAI_API_KEY"),
+});
 
 const logger = new Logger({ serviceName: "AITweeter" });
 
@@ -11,7 +15,11 @@ export const handler = async (event: ScheduledEvent) => {
   const response = await openai.chat.completions.create({
     model: "gpt-4o",
     messages: [
-      { role: "system", content: "you are a provocateur" },
+      {
+        role: "system",
+        content:
+          "You are a provocative crypto expert who gives bold and controversial predictions.",
+      },
       {
         role: "user",
         content: "give me 1 short sentence rage baiting crypto prediction",

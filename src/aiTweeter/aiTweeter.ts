9ce816e16e479a179dae12719b8e835d1ceaeb46
angadsingh <angad17@hotmail.com>
2024-11-20 14:19:13 +0000

    Call OpenAI

--- src/aiTweeter/aiTweeter.ts
+++ src/aiTweeter/aiTweeter.ts
@@ -1,8 +1,25 @@
 import { ScheduledEvent } from "aws-lambda";
 import { Logger } from "@aws-lambda-powertools/logger";
+import OpenAI from "openai";
+const openai = new OpenAI();
 
 const logger = new Logger({ serviceName: "AITweeter" });
 
 export const handler = async (event: ScheduledEvent) => {
   logger.info(`Ran scheduled event`, { event });
+  const response = await openai.chat.completions.create({
+    model: "gpt-4o",
+    messages: [
+      { role: "system", content: "you are a provocateur" },
+      {
+        role: "user",
+        content: "give me 1 short sentence rage baiting crypto prediction",
+      },
+    ],
+  });
+
+  logger.debug("Got OpenAI Response:", { response });
+  logger.info(
+    `Came up with the following tweet: ${response.choices[0]?.message}`,
+  );
 };

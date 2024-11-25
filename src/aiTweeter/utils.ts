5c307dc6a7a8aea2621d7debfea2e88c0b6481f5
angadsingh <angad17@hotmail.com>
2024-11-25 15:52:11 +0000

    Use Fisher-Yates shuffle for randomising elements

--- src/aiTweeter/utils.ts
+++ src/aiTweeter/utils.ts
@@ -1,11 +1,19 @@
 import { basePrompts, systemRoles, twitterUsers } from "./constants";
 import { fetchLastTweetForUser } from "../utils/twitterBot";
 
-const getRandomElements = <T>(array: T[], limit: number): T[] => {
-  const shuffled = [...array].sort(() => Math.random() - 0.5);
-  return shuffled.slice(0, limit);
+const shuffleArray = <T>(array: T[]): T[] => {
+  const shuffled = [...array];
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j] as T, shuffled[i] as T];
+  }
+  return shuffled;
 };
 
+const getRandomElements = <T>(array: T[], limit: number): T[] => {
+  const shuffled = shuffleArray(array);
+  return shuffled.slice(0, limit);
+};
 type Tweet = {
   handle: string;
   text: string;
